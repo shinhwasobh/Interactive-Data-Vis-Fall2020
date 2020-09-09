82039ee6c51ed5d93f8c894a9dec6c5e91ef48ef
Byeong Hyeon So <bso@gradcenter.cuny.edu>
2020-09-08 23:17:37 -0400

    Tutorial 1

---
+++ tutorial1_intro/main.js
@@ -0,0 +1,31 @@
+d3.csv("../../data/TFR_Korea.csv").then(data => {
+    console.log("data", data);
+
+    const table = d3.selectAll("#cities");  
+    const thead = table.append("thead");
+    thead
+        .append("tr")
+        .append("th")
+        .attr("colspan", "7")
+        .text("Annual Survey Results");
+
+    thead
+        .append("tr")
+        .selectAll("th")
+        .data(data.columns)
+        .join("td")
+        .text(d => d);
+    
+    const rows = table
+        .append("tbody")
+        .selectAll("tr")
+        .data(data)
+        .join("tr");
+
+    rows
+        .selectAll("td")
+        .data(d => Object.values(d))
+        .join("td")
+        .attr("class", d => +d > 0.6 ? 'Low' : null)
+        .text(d => d);
+});
