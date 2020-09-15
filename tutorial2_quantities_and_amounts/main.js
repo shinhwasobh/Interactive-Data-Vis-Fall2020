ca6328d83a0ca807aef9e2b9f4003d2c72aa74b7
Byeong Hyeon So <bso@gradcenter.cuny.edu>
2020-09-15 18:04:38 -0400

    Fruits Bar Graph

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -1,34 +1,45 @@
-d3.csv("../data/Fruit.csv").then
+d3.csv("../data/Fruit.csv", d3.autoType).then
 (data => {
-    const svgWidth = 500
-    const svgHeight = 500
 
     console.log(data)
-    console.log(data.map(d => d.activity))
+    console.log(data.map(d => d.Fruits));
+
+    const width = window.innerWidth * 0.8, 
+        height = window.innerHeight / 2,
+        paddingInner = 0.1,
+        margin = {top: 20, bottom: 20, left: 250, right: 10};
 
     const xScale = d3.scaleBand()
-        .domain(data.map(d => d.activity))
-        .range([0, svgWidth])
-        .paddingInner(0.2);
-
-    console.log(xScale('eating'), xScale.bandwidth())
+        .domain(data.map(d => d.Fruits))
+        .range([margin.left, width - margin.right])
+        .paddingInner(paddingInner);
+    
+    //console.log(xScale('Apple'), xScale.bandwidth())
 
     const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data.map(d => d.count))])
-        .range([svgHeight, 0]);
-    
-    const svg = d3.select('#my-svg')
+        .domain([0, d3.max(data.map(d => d.Sales_qty))])
+        .range([margin.bottom, height - margin.top]);
+        
+    const svg = d3.select("#my-svg")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-    const bars = svg.selectAll('rect.bar')
+    const rect = svg.selectAll("rect")
         .data(data)
-        .join('rect')
-        .attr('class', 'bar')
-        .attr('height', d => svgHeight - yScale(d.count))
-        //.attr('width', svgWidth / data.length)
-        .attr('width', d => xScale.bandwidth())
-        .attr("x", d => xScale(d.activity))
-        .attr("y", d => yScale(d.count))
-        .style("fill", "pink")
+        .join("rect")
+        .attr("y", d => height - yScale(d.Sales_qty))
+        .attr("x", d => xScale(d.Fruits))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => height - margin.bottom)
+        .attr("fill", "skyblue");
 
+    const text = svg.selectAll("text")
+        .data(data)
+        .join("text")
+        .attr("class", "label")
+        .text(d => d.Fruits)
+        .attr("x", d => xScale(d.Fruits) + (xScale.bandwidth() - 70))
+        .attr("y", d => height - yScale(d.Sales_qty));
+    
 })
-
