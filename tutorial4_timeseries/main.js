4c66b60d3f2d5d44d0ac58e68332da6c74c2c55d
Byeong Hyeon So <bso@gradcenter.cuny.edu>
2020-10-27 20:40:26 -0400

    Tutorial4_Timeseries

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -1,81 +1,49 @@
-/* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 },
-  radius = 5;
+'use strict';
+const width = window.innerwidth * 0.8,
+    height = window.innerheight * 0.8,
+    margin = {top: 20, bottom: 40, right: 30, left: 30}
+    radius = 4;
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
 let svg;
 let xScale;
 let yScale;
+let yAxis;
 
-/* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All", // + YOUR FILTER SELECTION
+  selectedSchool: null,
 };
 
-/* LOAD DATA */
-// + SET YOUR DATA PATH
-d3.json(YOUR_DATA_PATH, d3.autoType).then(raw_data => {
+d3.csv("../data/ELA_Queens.csv", d => ({
+  year: new Date(d.Year, 0, 1),
+  school: d.School,
+  score: d.avg_score,
+})).then(raw_data => {
   console.log("raw_data", raw_data);
   state.data = raw_data;
   init();
 });
 
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
+//Initialize the function
 function init() {
-  // + SCALES
+  xScale = d3
+    .scaleTime()
+    .domain(d3.extent(state.data, d = d.year))
+    .range([margin.left, width-margin.right]);
 
-  // + AXES
+  yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(state.data, d = d.avg_score))
+    .range([height - margin.bottom, margin.top]);
 
-  // + UI ELEMENT SETUP
+  const xAxis = d3.axisBottom(xScale);
+        yAxis = d3.axisLeft(yScale).tickFormat();
 
-  const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-    state.selection = this.value; // + UPDATE STATE WITH YOUR SELECTED VALUE
-    console.log("new value is", this.value);
-    draw(); // re-draw the graph based on this new selection
-  });
+//UI Element Setup
+  const selectElement = d3.select("#dropdown")
+                          .on("change", function(){
+                            console.log("new selected school is", this.value);
+                            
+                          }) 
 
-  // add in dropdown options from the unique values in the data
-  selectElement
-    .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD DATA VALUES FOR DROPDOWN
-    .join("option")
-    .attr("value", d => d)
-    .text(d => d);
-
-  // + SET SELECT ELEMENT'S DEFAULT VALUE (optional)
-
-  // + CREATE SVG ELEMENT
-
-  // + CALL AXES
-
-  draw(); // calls the draw function
-}
-
-/* DRAW FUNCTION */
-// we call this everytime there is an update to the data/state
-function draw() {
-  // + FILTER DATA BASED ON STATE
-  //
-  // + UPDATE SCALE(S), if needed
-  //
-  // + UPDATE AXIS/AXES, if needed
-  //
-  // + DRAW CIRCLES, if you decide to
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
-  //
-  // + DRAW LINE AND AREA
 }
