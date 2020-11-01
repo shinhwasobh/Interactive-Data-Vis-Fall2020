eaa7455103bbf7d6d90667c6ae3fbd936539b93f
Byeong Hyeon So <bso@gradcenter.cuny.edu>
2020-10-31 23:34:30 -0400

    Tutorial 5

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -1,58 +1,87 @@
-/**
- * CONSTANTS AND GLOBALS
- * */
-const width = window.innerWidth * 0.9,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 };
+'use strict';
 
-/** these variables allow us to access anything we manipulate in
- * init() but need access to in draw().
- * All these variables are empty before we assign something to them.*/
+const width = window.innerWidth * 0.8,
+      height = window.innerHeight * 0.8,
+      margin = {top: 30, bottom: 20, left: 20, right: 40};
+
 let svg;
 
-/**
- * APPLICATION STATE
- * */
 let state = {
-  // + SET UP STATE
+  geojson: null,
+  tfr: null,
+  hover: {longitude: null,
+          latitude: null,
+        state: null},
 };
 
-/**
- * LOAD DATA
- * Using a Promise.all([]), we can load more than one dataset at a time
- * */
-Promise.all([
-  d3.json("PATH_TO_YOUR_GEOJSON"),
-  d3.csv("PATH_TO_ANOTHER_DATASET", d3.autoType),
-]).then(([geojson, otherData]) => {
-  // + SET STATE WITH DATA
-  console.log("state: ", state);
-  init();
-});
+Promise.all([d3.json("../data/ny.json"),
+            d3.csv("../data/TFR_NY.csv", d3.autoType)])
+        .then([geojson, tfr]) => {
+          state.geojson = geojson;
+          state.tfr = tfr;
+          console.log("state: ", state);
+          init();
+        }
 
-/**
- * INITIALIZING FUNCTION
- * this will be run *one time* when the data finishes loading in
- * */
-function init() {
-  // create an svg element in our main `d3-container` element
-  svg = d3
-    .select("#d3-container")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
 
-  // + SET UP PROJECTION
-  // + SET UP GEOPATH
 
-  // + DRAW BASE MAP PATH
-  // + ADD EVENT LISTENERS (if you want)
 
-  draw(); // calls the draw function
-}
 
-/**
- * DRAW FUNCTION
- * we call this everytime there is an update to the data/state
- * */
-function draw() {}
+// /**
+//  * CONSTANTS AND GLOBALS
+//  * */
+// const width = window.innerWidth * 0.9,
+//   height = window.innerHeight * 0.7,
+//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+
+// /** these variables allow us to access anything we manipulate in
+//  * init() but need access to in draw().
+//  * All these variables are empty before we assign something to them.*/
+// let svg;
+
+// /**
+//  * APPLICATION STATE
+//  * */
+// let state = {
+//   // + SET UP STATE
+// };
+
+// /**
+//  * LOAD DATA
+//  * Using a Promise.all([]), we can load more than one dataset at a time
+//  * */
+// Promise.all([
+//   d3.json("PATH_TO_YOUR_GEOJSON"),
+//   d3.csv("PATH_TO_ANOTHER_DATASET", d3.autoType),
+// ]).then(([geojson, otherData]) => {
+//   // + SET STATE WITH DATA
+//   console.log("state: ", state);
+//   init();
+// });
+
+// /**
+//  * INITIALIZING FUNCTION
+//  * this will be run *one time* when the data finishes loading in
+//  * */
+// function init() {
+//   // create an svg element in our main `d3-container` element
+//   svg = d3
+//     .select("#d3-container")
+//     .append("svg")
+//     .attr("width", width)
+//     .attr("height", height);
+
+//   // + SET UP PROJECTION
+//   // + SET UP GEOPATH
+
+//   // + DRAW BASE MAP PATH
+//   // + ADD EVENT LISTENERS (if you want)
+
+//   draw(); // calls the draw function
+// }
+
+// /**
+//  * DRAW FUNCTION
+//  * we call this everytime there is an update to the data/state
+//  * */
+// function draw() {}
