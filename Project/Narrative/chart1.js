d64e25d23410064ba4b3df38191556d9cab48bbc
Byeong Hyeon So <bso@gradcenter.cuny.edu>
2020-12-11 21:23:01 -0500

    Narrative

---
+++ Project/Narrative/chart1.js
@@ -0,0 +1,78 @@
+export function chart1() {
+    
+    d3.csv("../data/Final_data.csv", d3.autoType).then
+    (data => {
+        console.log(data)
+        console.log(data.map(d => d.city));
+        debugger;
+        
+        const width = window.innerWidth / 2, 
+            height = window.innerHeight / 2,
+            paddingInner = 0.1,
+            margin = {top: 20, bottom: 40, left: 40, right: 40};
+            //colors = ["blue", "red", "yellow", "purple", "orange", "darkgreen", "pink", "lightgreen", "darkgreen", "beige", "yellow"];
+    
+        const xScale = d3.scaleLinear()
+            .domain([0, d3.max(data.map(d => d.tfr))])
+            .range([margin.left, width - margin.right])
+            .paddingInner(paddingInner);
+    
+        const yScale = d3.scaleLinear()
+            .domain(data.map(d => d.city))
+            .range([height - margin.bottom, margin.top]);
+
+        const yAxis = d3.axisLeft(yScale);
+        const xAxisBottom = dx.axisBottom(xScale).ticks(5);
+        const xAxisTop = d3.axisTop(xScale).ticks(5);
+
+        const mycolor = d3.scaleOrdinal()
+            .domain(data)
+            .range(colors);
+            
+        const svg = d3.select("#d3-container-1")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+            
+        const rect = svg.selectAll("rect")
+            .data(data)
+            .join("rect")
+            .attr("y", d => height - yScale(d.city))
+            .attr("x", d => margin.left)
+            .attr("width", d => xScale(d.tfr))
+            .attr("height", yScale.bandwidth())
+            .style("fill", function(d){return mycolor(d.city);
+            });
+        
+        const text = svg.selectAll("text")
+            .data(data)
+            .join("text")
+            .attr("class", "label")
+            .text(d => d.city)
+            .attr("x", margin.left)
+            .attr("y", d => height - yScale(d.Sales_qty));
+        
+        svg
+            .append("g")
+            .attr("class", "y-axis")
+            .attr("transform", `translate(${margin.left}, 0)`)
+            .call(yAxis);
+      
+        svg
+            .append("g")
+            .attr("class", "x-axis1")
+            .attr("transform", `translate(0, ${height - margin.bottom})`)
+            .call(xAxisBottom)
+            .append("text")
+            .attr("class", "axis-label")
+            .attr("x", width / 2)
+            .attr("dy", "3em")
+            .text("Total Fertility Rate");
+      
+        svg
+            .append("g")
+            .attr("class", "x-axis2")
+            .attr("transform", `translate(0, ${margin.top})`)
+            .call(xAxisTop);
+    })
+}
